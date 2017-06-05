1161626e4bba3c59c6861f51a45871bad3074114
Remy Oukaour <remy.oukaour@gmail.com>
2017-06-05 13:01:41 -0400

    Map header formatting

--- constants/map_constants.asm
+++ constants/map_constants.asm
@@ -1,11 +1,12 @@
-GROUP_N_A  EQU -1
-MAP_N_A    EQU -1
+GROUP_N_A EQU -1
+MAP_N_A EQU -1
 GROUP_NONE EQU 0
-MAP_NONE   EQU 0
+MAP_NONE EQU 0
 
 ; map group ids
 	const_def
-	newgroup                                                    ;  1
+	newgroup ; 1
+
 	mapgroup SUNBURST_ISLAND, 24, 20
 	mapgroup ROUTE_33, 9, 31
 	mapgroup ROUTE54, 24, 11
@@ -21,27 +22,26 @@ MAP_NONE   EQU 0
 	mapgroup SUNBURST_LARGE_HOUSE, 4, 4
 	mapgroup SUNBURST_SOUTH_HOUSE, 4, 4
 	mapgroup SUNBURST_CRYSTAL_SHOP, 4, 4
-	
 
-	newgroup                                                    ;  3
+	newgroup ; 2
 
-	mapgroup NATIONAL_PARK,                              27, 20 ; 15
-	mapgroup NATIONAL_PARK_BUG_CONTEST,                  27, 20 ; 16
+	mapgroup NATIONAL_PARK, 27, 20
+	mapgroup NATIONAL_PARK_BUG_CONTEST, 27, 20
 	mapgroup ILEX_FOREST, 27, 15
-	mapgroup WAREHOUSE_ENTRANCE,                         18, 15 ; 53
-	mapgroup UNDERGROUND_PATH_SWITCH_ROOM_ENTRANCES,     18, 15 ; 54
-	mapgroup ICE_PATH_1F,                                18, 20 ; 61
-	mapgroup ICE_PATH_B1F,                               18, 10 ; 62
-	mapgroup ICE_PATH_B2F_MAHOGANY_SIDE,                  9, 10 ; 63
-	mapgroup ICE_PATH_B2F_BLACKTHORN_SIDE,                9,  5 ; 64
-	mapgroup ICE_PATH_B3F,                                9, 10 ; 65
+	mapgroup WAREHOUSE_ENTRANCE, 18, 15
+	mapgroup UNDERGROUND_PATH_SWITCH_ROOM_ENTRANCES, 18, 15
+	mapgroup ICE_PATH_1F, 18, 20
+	mapgroup ICE_PATH_B1F, 18, 10
+	mapgroup ICE_PATH_B2F_MAHOGANY_SIDE, 9, 10
+	mapgroup ICE_PATH_B2F_BLACKTHORN_SIDE, 9, 5
+	mapgroup ICE_PATH_B3F, 9, 10
 
-	newgroup                                                    ;  6
+	newgroup ; 3
 
-	mapgroup CINNABAR_POKECENTER_1F,                      4,  5 ;  1
-	mapgroup CINNABAR_POKECENTER_2F_BETA,                 4,  8 ;  2
+	mapgroup CINNABAR_POKECENTER_1F, 4, 5
+	mapgroup CINNABAR_POKECENTER_2F_BETA, 4, 8
 
-	newgroup                                                    ;  7
+	newgroup ; 4
 
 	mapgroup CRYSTAL_CAVE_1F, 16, 20
 	mapgroup CRYSTAL_CAVE_B1, 16, 20
@@ -53,78 +53,78 @@ MAP_NONE   EQU 0
 	mapgroup ROUTE_56_POKE_CENTER, 4, 5
 	mapgroup KINNOW_ISLAND, 18, 20
 
-	newgroup                                                    ; 10
+	newgroup ; 5
 
-	mapgroup ROUTE_34_ILEX_FOREST_GATE,                   4,  5
+	mapgroup ROUTE_34_ILEX_FOREST_GATE, 4, 5
 	mapgroup ROUTE_32, 45, 11
 	mapgroup VIOLET_CITY, 18, 17
-	mapgroup VIOLET_MART,                                 4,  6 ;  6
+	mapgroup VIOLET_MART, 4, 6
 	mapgroup VIOLET_GYM, 9, 5
-	mapgroup VIOLET_NICKNAME_SPEECH_HOUSE,                4,  4 ;  9
-	mapgroup VIOLET_POKECENTER_1F,                        4,  5 ; 10
-	mapgroup VIOLET_ONIX_TRADE_HOUSE,                     4,  4 ; 11
+	mapgroup VIOLET_NICKNAME_SPEECH_HOUSE, 4, 4
+	mapgroup VIOLET_POKECENTER_1F, 4, 5
+	mapgroup VIOLET_ONIX_TRADE_HOUSE, 4, 4
 	mapgroup ROUTE_32_RUINS_OF_ALPH_GATE, 4, 5
 	mapgroup ROUTE_32_POKECENTER_1F, 4, 5
 
-	newgroup                                                    ; 11
+	newgroup ; 6
 
-	mapgroup DAYCARE,                                     4,  5 ; 24
+	mapgroup DAYCARE, 4, 5
 
-	newgroup                                                    ; 12
+	newgroup ; 7
 
-	mapgroup REDS_HOUSE_1F,                               4,  4 ;  3
-	mapgroup REDS_HOUSE_2F,                               4,  4 ;  4
-	mapgroup BLUES_HOUSE,                                 4,  4 ;  5
-	mapgroup OAKS_LAB,                                    6,  5 ;  6
+	mapgroup REDS_HOUSE_1F, 4, 4
+	mapgroup REDS_HOUSE_2F, 4, 4
+	mapgroup BLUES_HOUSE, 4, 4
+	mapgroup OAKS_LAB, 6, 5
 
-	newgroup                                                    ; 14
+	newgroup ; 8
 
 	mapgroup VALENCIA_PORT, 16, 10
-	mapgroup TANGELO_PORT,                             16, 10 ;  2
+	mapgroup TANGELO_PORT, 16, 10
 	mapgroup OLIVINE_PORT_PASSAGE, 9, 11
-	mapgroup VERMILION_PORT_PASSAGE,                      9, 10 ;  9
+	mapgroup VERMILION_PORT_PASSAGE, 9, 10
 
-	newgroup                                                    ; 16
+	newgroup ; 9
 
-	mapgroup HALL_OF_FAME,                                7,  5 ;  8
+	mapgroup HALL_OF_FAME, 7, 5
 
-	newgroup                                                    ; 17
+	newgroup ; 10
 
-	mapgroup POKECENTER_2F,                               4,  8 ;  1
-	mapgroup TRADE_CENTER,                                4,  5 ;  2
-	mapgroup COLOSSEUM,                                   4,  5 ;  3
-	mapgroup TIME_CAPSULE,                                4,  5 ;  4
-	mapgroup MOBILE_TRADE_ROOM_MOBILE,                    4,  5 ;  5
-	mapgroup MOBILE_BATTLE_ROOM,                          4,  5 ;  6
+	mapgroup POKECENTER_2F, 4, 8
+	mapgroup TRADE_CENTER, 4, 5
+	mapgroup COLOSSEUM, 4, 5
+	mapgroup TIME_CAPSULE, 4, 5
+	mapgroup MOBILE_TRADE_ROOM_MOBILE, 4, 5
+	mapgroup MOBILE_BATTLE_ROOM, 4, 5
 
-	newgroup                                                    ; 21
-	
-	mapgroup BATTLE_TOWER_1F,                             5,  8 ; 11
-	mapgroup BATTLE_TOWER_BATTLE_ROOM,                    4,  4 ; 12
-	mapgroup BATTLE_TOWER_ELEVATOR,                       2,  2 ; 13
-	mapgroup BATTLE_TOWER_HALLWAY,                        2, 11 ; 14
-	mapgroup ROUTE_40_BATTLE_TOWER_GATE,                  4,  5 ; 15
-	mapgroup BATTLE_TOWER_OUTSIDE,                       14, 10 ; 16
+	newgroup ; 11
 
-	newgroup                                                    ; 23
+	mapgroup BATTLE_TOWER_1F, 5, 8
+	mapgroup BATTLE_TOWER_BATTLE_ROOM, 4, 4
+	mapgroup BATTLE_TOWER_ELEVATOR, 2, 2
+	mapgroup BATTLE_TOWER_HALLWAY, 2, 11
+	mapgroup ROUTE_40_BATTLE_TOWER_GATE, 4, 5
+	mapgroup BATTLE_TOWER_OUTSIDE, 14, 10
+
+	newgroup ; 12
 
 	mapgroup ROUTE_29, 11, 20
 	mapgroup NEW_BARK_TOWN, 12, 14
 	mapgroup ELMS_LAB, 6, 5
 	mapgroup KRISS_HOUSE_1F, 4, 5
-	mapgroup KRISS_HOUSE_2F,                              3,  4 ;  7
-	mapgroup KRISS_NEIGHBORS_HOUSE,                       4,  4 ;  8
+	mapgroup KRISS_HOUSE_2F, 3, 4
+	mapgroup KRISS_NEIGHBORS_HOUSE, 4, 4
 	mapgroup ELMS_HOUSE, 4, 4
 
-	newgroup                                                    ; 25
+	newgroup ; 13
 
 	mapgroup ROUTE_30, 27, 10
 	mapgroup ROUTE_31, 15, 20
-	mapgroup ROUTE_31_VIOLET_GATE,                        4,  5 ; 11
+	mapgroup ROUTE_31_VIOLET_GATE, 4, 5
 	mapgroup CHERRYGROVE_CITY, 15, 20
-	mapgroup CHERRYGROVE_MART,                            4,  6 ;  4
-	mapgroup CHERRYGROVE_POKECENTER_1F,                   4,  5 ;  5
-	mapgroup CHERRYGROVE_GYM_SPEECH_HOUSE,                4,  4 ;  6
+	mapgroup CHERRYGROVE_MART, 4, 6
+	mapgroup CHERRYGROVE_POKECENTER_1F, 4, 5
+	mapgroup CHERRYGROVE_GYM_SPEECH_HOUSE, 4, 4
 	mapgroup CHERRYGROVE_EVOLUTION_SPEECH_HOUSE, 4, 4
 
 
@@ -173,39 +173,39 @@ const_value SET 1
 
 ; object struct
 	const_def
-	const OBJECT_SPRITE              ; 00
-	const OBJECT_MAP_OBJECT_INDEX    ; 01
-	const OBJECT_SPRITE_TILE         ; 02
-	const OBJECT_MOVEMENTTYPE        ; 03
-	const OBJECT_FLAGS1              ; 04
-	const OBJECT_FLAGS2              ; 05
-	const OBJECT_PALETTE             ; 06
-	const OBJECT_DIRECTION_WALKING   ; 07
-	const OBJECT_FACING              ; 08
-	const OBJECT_STEP_TYPE           ; 09
-	const OBJECT_STEP_DURATION       ; 0a
-	const OBJECT_ACTION              ; 0b
-	const OBJECT_STEP_FRAME          ; 0c
-	const OBJECT_FACING_STEP         ; 0d
-	const OBJECT_NEXT_TILE           ; 0e
-	const OBJECT_STANDING_TILE       ; 0f
-	const OBJECT_NEXT_MAP_X          ; 10
-	const OBJECT_NEXT_MAP_Y          ; 11
-	const OBJECT_MAP_X               ; 12
-	const OBJECT_MAP_Y               ; 13
-	const OBJECT_INIT_X              ; 14
-	const OBJECT_INIT_Y              ; 15
-	const OBJECT_RADIUS              ; 16
-	const OBJECT_SPRITE_X            ; 17
-	const OBJECT_SPRITE_Y            ; 18
-	const OBJECT_SPRITE_X_OFFSET     ; 19
-	const OBJECT_SPRITE_Y_OFFSET     ; 1a
+	const OBJECT_SPRITE ; 00
+	const OBJECT_MAP_OBJECT_INDEX ; 01
+	const OBJECT_SPRITE_TILE ; 02
+	const OBJECT_MOVEMENTTYPE ; 03
+	const OBJECT_FLAGS1 ; 04
+	const OBJECT_FLAGS2 ; 05
+	const OBJECT_PALETTE ; 06
+	const OBJECT_DIRECTION_WALKING ; 07
+	const OBJECT_FACING ; 08
+	const OBJECT_STEP_TYPE ; 09
+	const OBJECT_STEP_DURATION ; 0a
+	const OBJECT_ACTION ; 0b
+	const OBJECT_STEP_FRAME ; 0c
+	const OBJECT_FACING_STEP ; 0d
+	const OBJECT_NEXT_TILE ; 0e
+	const OBJECT_STANDING_TILE ; 0f
+	const OBJECT_NEXT_MAP_X ; 10
+	const OBJECT_NEXT_MAP_Y ; 11
+	const OBJECT_MAP_X ; 12
+	const OBJECT_MAP_Y ; 13
+	const OBJECT_INIT_X ; 14
+	const OBJECT_INIT_Y ; 15
+	const OBJECT_RADIUS ; 16
+	const OBJECT_SPRITE_X ; 17
+	const OBJECT_SPRITE_Y ; 18
+	const OBJECT_SPRITE_X_OFFSET ; 19
+	const OBJECT_SPRITE_Y_OFFSET ; 1a
 	const OBJECT_MOVEMENT_BYTE_INDEX ; 1b
-	const OBJECT_28                  ; 1c
-	const OBJECT_29                  ; 1d
-	const OBJECT_30                  ; 1e
-	const OBJECT_31                  ; 1f
-	const OBJECT_RANGE               ; 20
+	const OBJECT_28 ; 1c
+	const OBJECT_29 ; 1d
+	const OBJECT_30 ; 1e
+	const OBJECT_31 ; 1f
+	const OBJECT_RANGE ; 20
 ; 33-39 are not used
 
 ; map object struct
@@ -231,24 +231,24 @@ OBJECT_LENGTH EQU const_value
 MAPOBJECT_SCREEN_HEIGHT EQU 11
 MAPOBJECT_SCREEN_WIDTH EQU 12
 
-OW_DOWN  EQU DOWN  << 2
-OW_UP    EQU UP    << 2
-OW_LEFT  EQU LEFT  << 2
+OW_DOWN EQU DOWN << 2
+OW_UP EQU UP << 2
+OW_LEFT EQU LEFT << 2
 OW_RIGHT EQU RIGHT << 2
 
 	const_def
-	const EMOTE_SHOCK ; 0
-	const EMOTE_QUESTION ; 1
-	const EMOTE_HAPPY ; 2
-	const EMOTE_SAD ; 3
-	const EMOTE_HEART ; 4
-	const EMOTE_BOLT ; 5
-	const EMOTE_SLEEP ; 6
-	const EMOTE_FISH ; 7
-	const EMOTE_SHADOW ; 8
-	const EMOTE_ROD ; 9
-	const EMOTE_BOULDER_DUST ; 10
-	const EMOTE_0B ; 11
+	const EMOTE_SHOCK
+	const EMOTE_QUESTION
+	const EMOTE_HAPPY
+	const EMOTE_SAD
+	const EMOTE_HEART
+	const EMOTE_BOLT
+	const EMOTE_SLEEP
+	const EMOTE_FISH
+	const EMOTE_SHADOW
+	const EMOTE_ROD
+	const EMOTE_BOULDER_DUST
+	const EMOTE_0B
 EMOTE_MEM EQU -1
 
 	const_def
@@ -285,9 +285,9 @@ NUM_SPAWNS EQU const_value
 	const PALETTE_MORN
 	const PALETTE_DARK
 
-INVISIBLE    EQU 0
+INVISIBLE EQU 0
 FIXED_FACING EQU 2
-SLIDING      EQU 3
+SLIDING EQU 3
 EMOTE_OBJECT EQU 7
 
 	const_def
@@ -301,24 +301,24 @@ EMOTE_OBJECT EQU 7
 
 ; fruit trees
 const_value SET 1
-	const FRUITTREE_ROUTE_29      ; 01
-	const FRUITTREE_ROUTE_30_1    ; 02
-	const FRUITTREE_ROUTE_30_2    ; 05
-	const FRUITTREE_ROUTE_33      ; 06
-	const FRUITTREE_ROUTE_31      ; 07
-	const FRUITTREE_VIOLET_CITY   ; 09
+	const FRUITTREE_ROUTE_29
+	const FRUITTREE_ROUTE_30_1
+	const FRUITTREE_ROUTE_30_2
+	const FRUITTREE_ROUTE_33
+	const FRUITTREE_ROUTE_31
+	const FRUITTREE_VIOLET_CITY
 	const FRUITTREE_PINKAN_ISLAND_1
 	const FRUITTREE_PINKAN_ISLAND_2
 	const FRUITTREE_PINKAN_ISLAND_3
 	const FRUITTREE_PINKAN_ISLAND_4
-	
+
 NUM_FRUIT_TREES EQU const_value +- 1
 
-CMDQUEUE_TYPE  EQU 0
-CMDQUEUE_ADDR  EQU 1
-CMDQUEUE_03    EQU 3
-CMDQUEUE_04    EQU 4
-CMDQUEUE_05    EQU 5
+CMDQUEUE_TYPE EQU 0
+CMDQUEUE_ADDR EQU 1
+CMDQUEUE_03 EQU 3
+CMDQUEUE_04 EQU 4
+CMDQUEUE_05 EQU 5
 CMDQUEUE_ENTRY_SIZE EQU 6
 CMDQUEUE_CAPACITY EQU 4
 
