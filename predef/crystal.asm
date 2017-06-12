becdafcbd0eac4ec215d8058514aeca4ce5132dd
Remy Oukaour <remy.oukaour@gmail.com>
2017-06-11 23:47:46 -0400

    Refactor LoadSpecialMapPalette to be easier to extend

--- predef/crystal.asm
+++ predef/crystal.asm
@@ -1,3 +1,83 @@
+LoadSpecialMapPalette: ; 494ac
+	ld a, [wTileset]
+
+	ld hl, PokeComPalette
+	cp TILESET_POKECOM_CENTER
+	jr z, LoadEightBGPalettes
+
+	ld hl, BattleTowerPalette
+	cp TILESET_BATTLE_TOWER
+	jr z, LoadEightBGPalettes
+
+	ld hl, IcePathPalette
+	cp TILESET_ICE_PATH
+	jr z, .ice_path
+
+	ld hl, HousePalette
+	cp TILESET_HOUSE_1
+	jr z, LoadEightBGPalettes
+
+	ld hl, RadioTowerPalette
+	cp TILESET_RADIO_TOWER
+	jr z, LoadEightBGPalettes
+
+	ld hl, MansionPalette
+	cp TILESET_CELADON_MANSION
+	jr z, LoadEightBGPalettes
+
+.do_nothing
+	and a
+	ret
+
+.ice_path
+	ld a, [wPermission] ; permission
+	and $7
+	cp INDOOR ; Hall of Fame
+	jr z, .do_nothing
+	jr LoadEightBGPalettes
+; 494f2
+
+LoadEightTimeOfDayBGPalettes:
+	ld a, [TimeOfDayPal]
+	and $3
+	ld bc, 8 palettes
+	call AddNTimes
+	ld a, $5
+LoadEightBGPalettes: ; 494f2
+	ld a, $5
+	ld de, UnknBGPals
+	ld bc, 8 palettes
+	call FarCopyWRAM
+	scf
+	ret
+; 49501
+
+PokeComPalette: ; 49501
+INCLUDE "tilesets/pokecom.pal"
+; 49541
+
+BattleTowerPalette: ; 49550
+INCLUDE "tilesets/battle_tower.pal"
+; 49590
+
+IcePathPalette: ; 4959f
+INCLUDE "tilesets/ice_path.pal"
+; 495df
+
+HousePalette: ; 495ee
+INCLUDE "tilesets/house.pal"
+; 4962e
+
+RadioTowerPalette: ; 4963d
+INCLUDE "tilesets/radio_tower.pal"
+; 4967d
+
+MansionPalette: ; 496fe
+INCLUDE "tilesets/mansion.pal"
+; 49706
+
+
+
 LoadOW_BGPal7:: ; 49409
 	ld hl, Palette_TextBG7
 	ld de, UnknBGPals + 8 * 7
@@ -14,218 +94,19 @@ Palette_TextBG7: ; 49418
 	RGB 00, 00, 00
 ; 49420
 
-LoadSpecialMapPalette: ; 494ac
-	ld a, [wTileset]
-	cp TILESET_POKECOM_CENTER
-	jr z, .pokecom_2f
-	cp TILESET_BATTLE_TOWER
-	jr z, .battle_tower
-	cp TILESET_ICE_PATH
-	jr z, .ice_path
-	cp TILESET_HOUSE_1
-	jr z, .house
-	cp TILESET_RADIO_TOWER
-	jr z, .radio_tower
-	cp TILESET_CELADON_MANSION
-	jr z, .mansion_mobile
-	jr .do_nothing
 
-.pokecom_2f
-	call LoadPokeComPalette
-	scf
-	ret
-
-.battle_tower
-	call LoadBattleTowerPalette
-	scf
-	ret
-
-.ice_path
-	ld a, [wPermission] ; permission
-	and 7
-	cp 3 ; Hall of Fame
-	jr z, .do_nothing
-	call LoadIcePathPalette
-	scf
-	ret
-
-.house
-	call LoadHousePalette
-	scf
-	ret
-
-.radio_tower
-	call LoadRadioTowerPalette
-	scf
-	ret
-
-.mansion_mobile
-	call LoadMansionPalette
-	scf
-	ret
-
-.do_nothing
-	and a
-	ret
-; 494f2
-
-LoadPokeComPalette: ; 494f2
-	ld a, $5
-	ld de, UnknBGPals
-	ld hl, PokeComPalette
-	ld bc, 8 palettes
-	call FarCopyWRAM
-	ret
-; 49501
-
-PokeComPalette: ; 49501
-INCLUDE "tilesets/pokecom.pal"
-; 49541
-
-LoadBattleTowerPalette: ; 49541
-	ld a, $5
-	ld de, UnknBGPals
-	ld hl, BattleTowerPalette
-	ld bc, 8 palettes
-	call FarCopyWRAM
-	ret
-; 49550
-
-BattleTowerPalette: ; 49550
-INCLUDE "tilesets/battle_tower.pal"
-; 49590
-
-LoadIcePathPalette: ; 49590
-	ld a, $5
-	ld de, UnknBGPals
-	ld hl, IcePathPalette
-	ld bc, 8 palettes
-	call FarCopyWRAM
-	ret
-; 4959f
-
-IcePathPalette: ; 4959f
-INCLUDE "tilesets/ice_path.pal"
-; 495df
-
-LoadHousePalette: ; 495df
-	ld a, $5
-	ld de, UnknBGPals
-	ld hl, HousePalette
-	ld bc, 8 palettes
-	call FarCopyWRAM
-	ret
-; 495ee
-
-HousePalette: ; 495ee
-INCLUDE "tilesets/house.pal"
-; 4962e
-
-LoadRadioTowerPalette: ; 4962e
-	ld a, $5
-	ld de, UnknBGPals
-	ld hl, RadioTowerPalette
-	ld bc, 8 palettes
-	call FarCopyWRAM
-	ret
-; 4963d
-
-RadioTowerPalette: ; 4963d
-INCLUDE "tilesets/radio_tower.pal"
-; 4967d
-
-MansionPalette1: ; 4967d
-	RGB 30, 28, 26
-	RGB 19, 19, 19
-	RGB 13, 13, 13
-	RGB 07, 07, 07
-
-	RGB 30, 28, 26
-	RGB 31, 19, 24
-	RGB 30, 10, 06
-	RGB 07, 07, 07
-
-	RGB 18, 24, 09
-	RGB 15, 20, 01
-	RGB 09, 13, 00
-	RGB 07, 07, 07
-
-	RGB 30, 28, 26
-	RGB 15, 16, 31
-	RGB 09, 09, 31
-	RGB 07, 07, 07
-
-	RGB 30, 28, 26
-	RGB 31, 31, 07
-	RGB 31, 16, 01
-	RGB 07, 07, 07
-
-	RGB 26, 24, 17
-	RGB 21, 17, 07
-	RGB 16, 13, 03
-	RGB 07, 07, 07
-
-MansionPalette3: ; 496ad
-	RGB 30, 28, 26
-	RGB 17, 19, 31
-	RGB 14, 16, 31
-	RGB 07, 07, 07
-
-	RGB 31, 31, 16
-	RGB 31, 31, 16
-	RGB 14, 09, 00
-	RGB 00, 00, 00
-; 496bd
-
-MansionPalette4: ; 496bd
-	RGB 05, 05, 16
-	RGB 08, 19, 28
-	RGB 00, 00, 00
-	RGB 31, 31, 31
-; 496c5
-
-LoadMansionPalette: ; 496c5
-	ld a, $5
-	ld de, UnknBGPals
-	ld hl, MansionPalette1
-	ld bc, 8 palettes
-	call FarCopyWRAM
-	ld a, $5
-	ld de, UnknBGPals + 4 palettes
-	ld hl, MansionPalette2
-	ld bc, 1 palettes
-	call FarCopyWRAM
-	ld a, $5
-	ld de, UnknBGPals + 3 palettes
-	ld hl, MansionPalette3
-	ld bc, 1 palettes
-	call FarCopyWRAM
-	ld a, $5
-	ld de, UnknBGPals + 6 palettes
-	ld hl, MansionPalette4
-	ld bc, 1 palettes
-	call FarCopyWRAM
-	ret
-; 496fe
-
-MansionPalette2: ; 496fe
-	RGB 25, 24, 23
-	RGB 20, 19, 19
-	RGB 14, 16, 31
-	RGB 07, 07, 07
-; 49706
 
 LoadTradeRoomBGPals: ; 49811
-	ld hl, Palette_49826
-	ld de, UnknBGPals + $10
-	ld bc, $30
+	ld hl, TradeRoomBGPalette
+	ld de, UnknBGPals + 2 palettes
+	ld bc, 6 palettes
 	ld a, $5
 	call FarCopyWRAM
 	farcall ApplyPals
 	ret
 ; 49826
 
-Palette_49826: ; 49826
+TradeRoomBGPalette: ; 49826
 	RGB 04, 02, 15
 	RGB 07, 09, 31
 	RGB 31, 00, 00
@@ -256,23 +137,6 @@ Palette_49826: ; 49826
 	RGB 29, 28, 09
 	RGB 31, 31, 31
 ; 49856
-
-LinkTradeLayoutFillBox: ; 49336
-.row
-	push bc
-	push hl
-.col
-	ld [hli], a
-	dec c
-	jr nz, .col
-	pop hl
-	ld bc, SCREEN_WIDTH
-	add hl, bc
-	pop bc
-	dec b
-	jr nz, .row
-	ret
-; 49346
 
 InitLinkTradePalMap: ; 49856
 	hlcoord 0, 0, AttrMap
@@ -325,3 +189,20 @@ InitLinkTradePalMap: ; 49856
 	call ByteFill
 	ret
 ; 49811
+
+LinkTradeLayoutFillBox: ; 49336
+.row
+	push bc
+	push hl
+.col
+	ld [hli], a
+	dec c
+	jr nz, .col
+	pop hl
+	ld bc, SCREEN_WIDTH
+	add hl, bc
+	pop bc
+	dec b
+	jr nz, .row
+	ret
+; 49346
