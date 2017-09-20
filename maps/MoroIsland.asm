6143f3e3716a4b05000685d58b052fcc68e2fa76
PiaCarrot <piacarrot@live.com>
2017-09-20 17:03:29 -0400

    Moro Island Part 2

--- maps/MoroIsland.asm
+++ maps/MoroIsland.asm
@@ -6,8 +6,40 @@ MoroIsland_MapScriptHeader::
 
 .Scripts:
 
-.Text:
-
+MoroFattyScript:
+	jumptextfaceplayer MoroFattyText
+	
+MoroFattyText:
+	text "Technology is just"
+	line "amazing!"
+	
+	para "Hm? You're asking"
+	line "if I'm from VALEN-"
+	cont "CIA ISLAND?"
+	
+	para "I'm not, why?"
+	done
+	
+MoroCooltrainerMScript:
+	jumptextfaceplayer MoroCooltrainerMText
+	
+MoroCooltrainerMText:
+	text "Ever been to GOL-"
+	line "DEN ISLAND?"
+	
+	para "There are a bunch"
+	line "of crazy MEOWTH"
+	cont "lovers there!"
+	done
+	
+MoroCooltrainerFScript:
+	jumptextfaceplayer MoroCooltrainerFText
+	
+MoroCooltrainerFText:
+	text "I found the cutest"
+	line "#MON on ROUTE"
+	cont "60!"
+	done
 
 MoroIsland_MapEventHeader:: db 0, 0
 
@@ -18,5 +50,8 @@ MoroIsland_MapEventHeader:: db 0, 0
 
 .BGEvents: db 0
 
-.ObjectEvents: db 0
+.ObjectEvents: db 3
+	person_event SPRITE_COOLTRAINER_M, 21, 20, SPRITEMOVEDATA_STANDING_UP, 0, 1, -1, -2, PAL_OW_BLUE, PERSONTYPE_SCRIPT, 0, MoroCooltrainerMScript, -1
+	person_event SPRITE_COOLTRAINER_F, 20, 10, SPRITEMOVEDATA_STANDING_UP, 0, 1, -1, -2, PAL_OW_PURPLE, PERSONTYPE_SCRIPT, 0, MoroCooltrainerFScript, -1
+	person_event SPRITE_FISHER, 14, 17, SPRITEMOVEDATA_STANDING_UP, 0, 1, -1, -2, PAL_OW_RED, PERSONTYPE_SCRIPT, 0, MoroFattyScript, -1
 
