0e13517a10e75076ced078d30231d112c4af604c
Remy Oukaour <remy.oukaour@gmail.com>
2017-10-10 21:33:21 -0400

    Add possible Dive sprites

--- gfx/overworld/sprites_3.asm
+++ gfx/overworld/sprites_3.asm
@@ -1,9 +1,9 @@
-SightSeerSpriteGFX:			  INCBIN "gfx/overworld/102.2bpp"
-BugBoySpriteGFX:			  INCBIN "gfx/overworld/103.2bpp"
-PcSlouchGFX:				  INCBIN "gfx/overworld/104.2bpp"
-JessieSpriteGFX:			  INCBIN "gfx/overworld/105.2bpp"
-JamesSpriteGFX:				  INCBIN "gfx/overworld/106.2bpp"
-CatManSpriteGFX:			  INCBIN "gfx/overworld/107.2bpp"
-RocketMeowthSpriteGFX:		  INCBIN "gfx/overworld/108.2bpp"
-ChrisDiveSpriteGFX:			  INCBIN "gfx/overworld/106.2bpp" ; TODO: boy dive sprite
-KrisDiveSpriteGFX:			  INCBIN "gfx/overworld/105.2bpp" ; TODO: girl dive sprite
+SightSeerSpriteGFX:			INCBIN "gfx/overworld/102.2bpp"
+BugBoySpriteGFX:			INCBIN "gfx/overworld/103.2bpp"
+PcSlouchGFX:				INCBIN "gfx/overworld/104.2bpp"
+JessieSpriteGFX:			INCBIN "gfx/overworld/105.2bpp"
+JamesSpriteGFX:				INCBIN "gfx/overworld/106.2bpp"
+CatManSpriteGFX:			INCBIN "gfx/overworld/107.2bpp"
+RocketMeowthSpriteGFX:		INCBIN "gfx/overworld/108.2bpp"
+ChrisDiveSpriteGFX:			INCBIN "gfx/overworld/109.2bpp"
+KrisDiveSpriteGFX:			INCBIN "gfx/overworld/110.2bpp"
