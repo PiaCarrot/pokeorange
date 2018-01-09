9b68fd72311581237d09cd4bb312475af90f756a
PiaCarrot <piacarrot@live.com>
2018-01-09 17:26:57 -0500

    Fukuhara No 4 Mapdata
    Will commit scripts and map headers later

---
+++ maps/FukuharaNo4BF1.asm
@@ -0,0 +1,17 @@
+const_value set 1
+
+FukuharaNo4BF1_MapScriptHeader::
+
+.Triggers: db 0
+
+.Callbacks: db 0
+
+FukuharaNo4BF1_MapEventHeader::
+
+.Warps: db 0
+
+.CoordEvents: db 0
+
+.BGEvents: db 0
+
+.ObjectEvents: db 0
