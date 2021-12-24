7bd09889cce02109da9ade358cbd2bba3c892aca
PiaCarrot <PiaCarrot@github.com>
2021-12-24 14:27:40 -0500

    Seashell 6/50

--- maps/WreckedShip.asm
+++ maps/WreckedShip.asm
@@ -1,3 +1,6 @@
+const_value set 1
+	const WRECKED_SHIP_SEASHELL
+
 WreckedShip_MapScriptHeader::
 
 .Triggers: db 0
@@ -9,6 +12,38 @@ WreckedShip_MapScriptHeader::
 	divemap ROUTE_60, 0, 0
 	return
 
+WreckedShipSeashellScript:
+	opentext
+	checkitem SHELL_BOX
+	iffalse .NoShellBox
+	writetext FoundWreckedShipSeashell
+	playsound SFX_DEX_FANFARE_140_169
+	waitsfx
+	closetext
+	giveshells 1
+	setevent EVENT_WRECKED_SHIP_SEASHELL
+	disappear WRECKED_SHIP_SEASHELL
+	end
+	
+.NoShellBox:
+	writetext WreckedShipNoShellBox
+	waitbutton
+	closetext
+	end
+	
+WreckedShipNoShellBox:
+    text "What a pretty sea-"
+    line "shell! It looks"
+    cont "too fragile to"
+    cont "go in my BAG."
+    done
+	
+FoundWreckedShipSeashell:
+	text "You found a rare"
+	line "seashell!"
+	done
+
+
 WreckedShip_MapEventHeader::
 
 .Warps: db 1
@@ -18,5 +53,5 @@ WreckedShip_MapEventHeader::
 
 .BGEvents: db 0
 
-.ObjectEvents: db 0
-
+.ObjectEvents: db 1
+	person_event SPRITE_SEASHELL,  5,  4, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, PERSONTYPE_SCRIPT, 0, WreckedShipSeashellScript, EVENT_WRECKED_SHIP_SEASHELL
