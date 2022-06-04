386417e46b8f41fc646e65683eda5ca9a644b02d
PiaCarrot <PiaCarrot@github.com>
2022-06-04 19:00:00 -0400

    Melody and Chief Script

--- maps/ChiefsHouse.asm
+++ maps/ChiefsHouse.asm
@@ -6,6 +6,108 @@ ChiefsHouse_MapScriptHeader:
 
 .MapCallbacks: db 0
 
+ShamoutiChiefScript:
+	faceplayer
+	opentext
+	checkevent EVENT_SHAMOUTI_QUEST_STARTED
+	iftrue .AcceptedShamoutiQuest
+	writetext ShamoutiChiefText1
+	waitbutton
+	closetext
+	setevent EVENT_SHAMOUTI_QUEST_STARTED
+	end
+	
+.AcceptedShamoutiQuest
+	writetext ShamoutiChiefText2
+	waitbutton
+	closetext
+	end
+
+ShamoutiChiefText2:
+	text "If you'd like more"
+	line "details, please go"
+	cont "to my home just"
+	cont "outside of this"
+	cont "building. My kid"
+	cont "will fill you in."
+	done
+
+ShamoutiChiefText1:
+	text "CHIEF: Ah, are you"
+	line "a tourist?"
+	
+	para "<...>"
+	
+	para "CHIEF: So, your"
+	line "name is <PLAYER>,"
+	cont "and you are the"
+	cont "current CHAMPION"
+	
+	para "of the ORANGE"
+	line "CREW!"
+	
+	para "What wondrous"
+	line "timing!"
+	cont "You see, every"
+	cont "year we hold a"
+	
+	para "festival in honor"
+	line "of our guardians,"
+	cont "the gods of fire,"
+	cont "ice, lightning,"
+	cont "and the sea."
+	
+	para "They protect us,"
+	line "nay, the entire"
+	cont "world!"
+	
+	para "Anyway, in the"
+	line "legends, should"
+	cont "the gods be dis-"
+	cont "turbed, it is"
+	cont "said only a great"
+	cont "trainer can bring"
+	cont "balance back to"
+	cont "the world."
+	
+	para "Now, we'd normally"
+	line "let any trainer"
+	cont "do the part, but"
+	cont "there's been some"
+	cont "sightings of a big"
+	cont "flying castle as"
+	cont "of late."
+	
+	para "I'm worried about"
+	line "the guardian gods!"
+	cont "Please look into"
+	cont "this, CHAMPION!"
+	
+	para "If you'd like more"
+	line "details, please go"
+	cont "to the home just"
+	cont "outside of this"
+	cont "building. My kid"
+	cont "will fill you in."
+	done
+	
+ChiefSage1Script:
+	jumptextfaceplayer ChiefSage1Text
+	
+ChiefSage1Text:
+	text "<...>And she says"
+	line "'No, but I have"
+	cont "KRABBYs!' Wahaha!"
+	done
+	
+ChiefSage2Script:
+	jumptextfaceplayer ChiefSage2Text
+	
+ChiefSage2Text:
+	text "Hehehe<...>"
+	line "KRABBYs<...>"
+	done
+
 ChiefsHouse_MapEventHeader:
 
 .Warps: db 2
@@ -16,4 +118,7 @@ ChiefsHouse_MapEventHeader:
 
 .Signposts: db 0
 
-.PersonEvents: db 0
+.PersonEvents: db 3
+	person_event SPRITE_GRAMPS,  4, 11, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, (1 << 3) | PAL_OW_BROWN, PERSONTYPE_SCRIPT, 0, ShamoutiChiefScript, -1
+	person_event SPRITE_SAGE,  3,  2, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, (1 << 3) | PAL_OW_BROWN, PERSONTYPE_SCRIPT, 0, ChiefSage1Script, -1
+	person_event SPRITE_SAGE,  4,  5, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, (1 << 3) | PAL_OW_BROWN, PERSONTYPE_SCRIPT, 0, ChiefSage2Script, -1
--- maps/ShamoutiHouse.asm
+++ maps/ShamoutiHouse.asm
@@ -6,6 +6,57 @@ ShamoutiHouse_MapScriptHeader:
 
 .MapCallbacks: db 0
 
+MelodyShamouti:
+	checkevent EVENT_SHAMOUTI_QUEST_STARTED
+	iftrue .PlayLugiaSong
+	jumptextfaceplayer MelodyText1
+	end
+	
+.PlayLugiaSong
+	faceplayer
+	opentext
+	writetext MelodyText2
+	waitbutton
+	closetext
+	playmusic MUSIC_LUGIAS_SONG_2000
+	end
+	
+	
+MelodyText1:
+	text "MELODY: Are you"
+	line "here for the"
+	cont "Legend Festival?"
+	
+	para "My father is"
+	line "celebrating at"
+	cont "the house behind"
+	cont "this one."
+	
+	para "You should go see"
+	line "him."
+	done
+	
+MelodyText2:
+	text "MELODY: So you're "
+	line "going to play the"
+	cont "role of trainer in"
+	cont "the festival?"
+	
+	para "All you gotta do"
+	line "is go to the 3"
+	cont "islands, grab the"
+	cont "treasures, and"
+	cont "place them on the"
+	cont "shrine. Then I'll"
+	cont "do the rest by"
+	cont "playing the song"
+	cont "of the sea god."
+	
+	para "Wanna hear it? It"
+	line "goes like this<...>"
+	done
+	
+
 ShamoutiHouse_MapEventHeader:
 
 .Warps: db 2
@@ -16,4 +67,5 @@ ShamoutiHouse_MapEventHeader:
 
 .Signposts: db 0
 
-.PersonEvents: db 0
+.PersonEvents: db 1
+	person_event SPRITE_JASMINE,  3,  2, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, (1 << 3) | PAL_OW_BROWN, PERSONTYPE_SCRIPT, 0, MelodyShamouti, EVENT_BEAT_LAWRENCE
