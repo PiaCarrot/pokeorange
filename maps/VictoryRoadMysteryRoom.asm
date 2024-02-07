dc265f2fd65890b849a20fefb8c8dd6f3a6b53ed
suloku <sulokutdcmago@gmail.com>
2024-02-07 21:06:15 +0100

    - Marshadow added

--- maps/VictoryRoadMysteryRoom.asm
+++ maps/VictoryRoadMysteryRoom.asm
@@ -1,4 +1,25 @@
 const_value = 1
+	const MARSHADOW_VICTORY_ROAD_MISTERY_ROOM
+
+VictoryRoadMysteryRoomMarshadowScript:	
+	faceplayer
+	opentext
+	writetext MarshadowText
+	cry MARSHADOW
+	pause 15
+	closetext
+	writecode VAR_BATTLETYPE, BATTLETYPE_SNORLAX
+	loadwildmon MARSHADOW, 50
+	startbattle
+	disappear MARSHADOW_VICTORY_ROAD_MISTERY_ROOM
+	reloadmapafterbattle
+	playmapmusic
+	setevent EVENT_VICTORY_ROAD_MARSHADOW_FOUGHT
+	end
+	
+MarshadowText:
+	text "Shadow!"
+	done
 
 VictoryRoadMysteryRoom_MapScriptHeader::
 
@@ -15,4 +36,5 @@ VictoryRoadMysteryRoom_MapEventHeader::
 
 .BGEvents: db 0
 
-.ObjectEvents: db 0
+.ObjectEvents: db 1
+	person_event SPRITE_MARSHADOW,  2,  4, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_OW_GRAY, 0, 0, VictoryRoadMysteryRoomMarshadowScript, EVENT_VICTORY_ROAD_MARSHADOW_FOUGHT
