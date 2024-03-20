92fa66e28d34845eeb441d68030cefcfab123393
suloku <sulokutdcmago@gmail.com>
2024-03-20 11:13:11 +0100

    Forgot celebi shrine sprites -added batch file to build all versions

---
+++ release.bat
@@ -0,0 +1,12 @@
+make clean
+make
+mv pokeorange.gbc pokeorange-nopss.gbc
+make clean
+make debug
+mv pokeorange.gbc pokeorange-nopss-debug.gbc
+make clean
+make pss
+mv pokeorange.gbc pokeorange-pss.gbc
+make clean
+make pss debug
+mv pokeorange.gbc pokeorange-pss-debug.gbc
