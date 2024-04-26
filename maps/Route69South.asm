a65d0090aabf8c18fc4bcf48ef497485ee0b9dd5
suloku <sulokutdcmago@gmail.com>
2024-04-26 20:55:05 +0200

    - Add rock-climb naviagtion shortcuts - Add healer npc at VR's entrance

--- maps/Route69South.asm
+++ maps/Route69South.asm
@@ -4,6 +4,7 @@ const_value = 1
 	const ROUTE_69_POKEBALL_3
 	const ROUTE_69_COOLTRAINER_MAKO
 	const ROUTE_69_POKEBALL_4
+	const ROUTE_69_NURSE
 
 Route69South_MapScriptHeader::
 
@@ -59,6 +60,43 @@ CooltrainerMMakoAfterText:
 Route69DragonPulse:
 	itemball TM_DRAGON_PULSE
 
+Route69NurseScript:
+	faceplayer
+	opentext
+	writetext Route69NurseText
+	waitbutton
+	closetext
+	special Special_FadeBlackQuickly
+	special Special_ReloadSpritesNoPalettes
+	playmusic MUSIC_HEAL
+	special HealParty
+	pause 60
+	special Special_FadeInQuickly
+	special RestartMapMusic
+	opentext
+	writetext Route69NurseHealedText
+	waitbutton
+	closetext
+	end
+
+Route69NurseText:
+	text "Hello trainer!"
+
+	para "Victory Road is"
+	line "just ahead, so be"
+	cont "sure you are fully"
+	cont "prepared!"
+	
+	para "Let me heal your"
+	line "party."
+	done
+
+Route69NurseHealedText:
+	text "Good luck."
+
+	para "You'll need it."
+	done
+
 Route69South_MapEventHeader::
 
 .Warps: db 15
@@ -83,11 +121,12 @@ Route69South_MapEventHeader::
 .BGEvents: db 1
 	signpost 44, 22, SIGNPOST_READ, VictoryRoadSign
 
-.ObjectEvents: db 5
+.ObjectEvents: db 6
 	person_event SPRITE_POKE_BALL, 60,  6, SPRITEMOVEDATA_ITEM_TREE, 0, 0, -1, -1, 0, PERSONTYPE_ITEMBALL, 0, Route69SouthUltraBall, EVENT_ROUTE_69_SOUTH_ULTRA_BALL
-	person_event SPRITE_POKE_BALL, 18, 11, SPRITEMOVEDATA_ITEM_TREE, 0, 0, -1, -1, 0, PERSONTYPE_ITEMBALL, 0, Route69SouthFullRestore, EVENT_ROUTE_69_SOUTH_FULL_RESTORE
+	person_event SPRITE_POKE_BALL, 19,  9, SPRITEMOVEDATA_ITEM_TREE, 0, 0, -1, -1, 0, PERSONTYPE_ITEMBALL, 0, Route69SouthFullRestore, EVENT_ROUTE_69_SOUTH_FULL_RESTORE
 	person_event SPRITE_POKE_BALL, 29, 34, SPRITEMOVEDATA_ITEM_TREE, 0, 0, -1, -1, 0, PERSONTYPE_ITEMBALL, 0, Route69SouthMaxEther, EVENT_ROUTE_69_SOUTH_MAX_ETHER
 	person_event SPRITE_COOLTRAINER_M, 23,  7, SPRITEMOVEDATA_SPINRANDOM_SLOW, 1, 0, -1, -1, (1 << 3) | PAL_OW_RED, PERSONTYPE_TRAINER, 3, TrainerCooltrainerMMako, -1
 	person_event SPRITE_POKE_BALL, 15, 30, SPRITEMOVEDATA_ITEM_TREE, 0, 0, -1, -1, 0, PERSONTYPE_ITEMBALL, 0, Route69DragonPulse, EVENT_ROUTE_69_DRAGON_PULSE
+	person_event SPRITE_NURSE, 44, 21, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, PERSONTYPE_SCRIPT, 0, Route69NurseScript, -1
 
 
