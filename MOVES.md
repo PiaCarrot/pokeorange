c562e33a39f225cea45f13d81ae1638b5802c995
PiaCarrot <PiaCarrot@github.com>
2024-05-19 14:31:50 -0400

    Punch move tutors

--- MOVES.md
+++ MOVES.md
@@ -78,16 +78,16 @@ Vicegrip
 
 ## TMs
 
-- TM01 *Power-Up Punch*
+- TM01 *Power-Up Punch* (Dept Store)
 - TM02 *Dragon Pulse* (Route 69)
 - TM03 *Water Pulse* (Route 55 Underwater)
-- TM04 *Dark Pulse*
-- TM05 *Venoshock*
-- TM06 Toxic
+- TM04 *Dark Pulse* (Sewer)
+- TM05 *Venoshock* (Sewer)
+- TM06 Toxic (Sewer)
 - TM07 *Hail* (Danny)
 - TM08 Whirlpool (Route 63)
 - TM09 *Bullet Seed* (Pinkan Island)
-- TM10 Fissure
+- TM10 Fissure (Unnamed Island 2)
 - TM11 Sunny Day (Sunburst Island)
 - TM12 Lock-On (Route 66)
 - TM13 Ice Beam (Trovitopolis Dept. Store)
@@ -104,24 +104,24 @@ Vicegrip
 - TM24 Thunderbolt (Trovitopolis Dept. Store)
 - TM25 Thunder (Game Corner)
 - TM26 Earthquake (Route 62)
-- TM27 Return (Show Lapras to its Family on Route 71)
-- TM28 Dig
+- TM27 Return (Show Lapras to its Family on Route 71, Unnamed Island 2)
+- TM28 Dig (Dept Store)
 - TM29 Psychic (Luana)
 - TM30 Shadow Ball (Wrecked Ship)
 - TM31 Bubblebeam (Cissy)
 - TM32 Double Team (Rudy)
-- TM33 *Earth Power*
-- TM34 *Giga Impact*
+- TM33 *Earth Power* (Dept Store)
+- TM34 *Giga Impact* (Dept Store)
 - TM35 Flamethrower (Trovitopolis Dept. Store)
 - TM36 Sludge Bomb (Trovitopolis Sewer)
 - TM37 Sandstorm (Mandarin Desert)
 - TM38 Fire Blast (Game Corner)
-- TM39 *Dazzling Gleam* 
+- TM39 *Dazzling Gleam*  (Dept Store)
 - TM40 *Aerial Ace* (Unnamed Island 3)
-- TM41 *Flash Cannon*
+- TM41 *Flash Cannon* (Dept Store)
 - TM42 *Facade* (Moro Island)
-- TM43 Zap Cannon
-- TM44 Rest
+- TM43 Zap Cannon (Dept Store)
+- TM44 Rest (Unnamed Island 2)
 - TM45 Seed Bomb (Route 67)
 - TM46 Thief (Mandarin North)
 - TM47 Steel Wing (Unnamed Island 1)
@@ -142,12 +142,12 @@ Vicegrip
 
 ## Tutors
 
-- MT01 Fire Punch
+- MT01 Fire Punch (Dept Store)
 - MT02 ThunderPunch (Route 52)
-- MT03 Ice Punch
-- MT04 Comet Punch
+- MT03 Ice Punch (Dept Store)
+- MT04 Comet Punch (Dept Store)
 - MT05 Mach Punch (Route 56)
-- MT06 DynamicPunch
+- MT06 DynamicPunch (Dept Store)
 - MT07 Headbutt (Moro Island)
 - MT08 *Zen Headbutt* (Route 69)
 - MT09 Swords Dance (Route 71)
--- maps/TrovitopolisDeptStore5F.asm
+++ maps/TrovitopolisDeptStore5F.asm
@@ -1,5 +1,9 @@
 const_value = 1
 	const TROVITOPOLISDEPTSTORE5F_CLERK
+	const TROVITOPOLISDEPTSTORE5F_BLACK_BELT_1
+	const TROVITOPOLISDEPTSTORE5F_BLACK_BELT_2
+	const TROVITOPOLISDEPTSTORE5F_BLACK_BELT_3
+	const TROVITOPOLISDEPTSTORE5F_BLACK_BELT_4
 
 TrovitopolisDeptStore5F_MapScriptHeader:
 
@@ -25,6 +29,218 @@ TrovitopolisTMMartScript:
 	pokemart MARTTYPE_STANDARD, MART_TROVITOPOLIS_TM
 	closetext
 	end
+	
+Route52IcepunchTutor:
+	faceplayer
+	opentext
+	writetext GivePlayerIcepunchText
+	yesorno
+	iffalse .TutorRefused
+	writebyte ICE_PUNCH
+	writetext Text_IcepunchTutorClear
+	special Special_MoveTutor
+	if_equal $0, .TeachMove
+.TutorRefused
+	writetext Text_IcepunchTutorRefused
+	waitbutton
+	closetext
+	end
+
+.TeachMove
+	writetext Text_IcepunchTutorTaught
+	waitbutton
+	closetext
+	end
+
+GivePlayerIcepunchText:
+	text "Hu ha! We are the"
+	line "Order of the Fist!"
+	
+	para "Our leader left us"
+	line "here to teach our"
+	cont "ways while he went"
+	cont "on a mission to"
+	cont "TANGELO ISLAND!"
+	
+	para "As such, I can"
+	line "teach your #MON"
+	cont "ICE PUNCH! Want"
+	cont "to learn?"
+	done
+	
+Text_IcepunchTutorTaught:
+	text "ICE PUNCH is"
+	line "an ICE attack"
+	cont "that can sometimes"
+	cont "freeze your foe!"
+	done
+	
+Text_IcepunchTutorRefused:
+	text "Very well!"
+	done
+	
+Text_IcepunchTutorClear:
+	text ""
+	done
+	
+Route52FirepunchTutor:
+	faceplayer
+	opentext
+	writetext GivePlayerFirepunchText
+	yesorno
+	iffalse .TutorRefused
+	writebyte FIRE_PUNCH
+	writetext Text_FirepunchTutorClear
+	special Special_MoveTutor
+	if_equal $0, .TeachMove
+.TutorRefused
+	writetext Text_FirepunchTutorRefused
+	waitbutton
+	closetext
+	end
+
+.TeachMove
+	writetext Text_FirepunchTutorTaught
+	waitbutton
+	closetext
+	end
+
+GivePlayerFirepunchText:
+	text "Hu ha! We are the"
+	line "Order of the Fist!"
+	
+	para "Our leader left us"
+	line "here to teach our"
+	cont "ways while he went"
+	cont "on a mission to"
+	cont "TANGELO ISLAND!"
+	
+	para "As such, I can"
+	line "teach your #MON"
+	cont "FIRE PUNCH! Want"
+	cont "to learn?"
+	done
+	
+Text_FirepunchTutorTaught:
+	text "FIRE PUNCH is"
+	line "a FIRE attack"
+	cont "that can sometimes"
+	cont "burn your foe!"
+	done
+	
+Text_FirepunchTutorRefused:
+	text "Very well!"
+	done
+	
+Text_FirepunchTutorClear:
+	text ""
+	done
+	
+Route52CometpunchTutor:
+	faceplayer
+	opentext
+	writetext GivePlayerCometpunchText
+	yesorno
+	iffalse .TutorRefused
+	writebyte COMET_PUNCH
+	writetext Text_CometpunchTutorClear
+	special Special_MoveTutor
+	if_equal $0, .TeachMove
+.TutorRefused
+	writetext Text_CometpunchTutorRefused
+	waitbutton
+	closetext
+	end
+
+.TeachMove
+	writetext Text_CometpunchTutorTaught
+	waitbutton
+	closetext
+	end
+
+GivePlayerCometpunchText:
+	text "Hu ha! We are the"
+	line "Order of the Fist!"
+	
+	para "Our leader left us"
+	line "here to teach our"
+	cont "ways while he went"
+	cont "on a mission to"
+	cont "TANGELO ISLAND!"
+	
+	para "As such, I can"
+	line "teach your #MON"
+	cont "COMET PUNCH! Want"
+	cont "to learn?"
+	done
+	
+Text_CometpunchTutorTaught:
+	text "COMET PUNCH is"
+	line "a NORMAL attack"
+	cont "that can hit the"
+	cont "foe many times!"
+	done
+	
+Text_CometpunchTutorRefused:
+	text "Very well!"
+	done
+	
+Text_CometpunchTutorClear:
+	text ""
+	done
+	
+Route52DynamicPunchTutor:
+	faceplayer
+	opentext
+	writetext GivePlayerDynamicpunchText
+	yesorno
+	iffalse .TutorRefused
+	writebyte DYNAMICPUNCH
+	writetext Text_DynamicpunchTutorClear
+	special Special_MoveTutor
+	if_equal $0, .TeachMove
+.TutorRefused
+	writetext Text_DynamicpunchTutorRefused
+	waitbutton
+	closetext
+	end
+
+.TeachMove
+	writetext Text_DynamicpunchTutorTaught
+	waitbutton
+	closetext
+	end
+
+GivePlayerDynamicpunchText:
+	text "Hu ha! We are the"
+	line "Order of the Fist!"
+	
+	para "Our leader left us"
+	line "here to teach our"
+	cont "ways while he went"
+	cont "on a mission to"
+	cont "TANGELO ISLAND!"
+	
+	para "As such, I can"
+	line "teach your #MON"
+	cont "DYNAMICPUNCH! Want"
+	cont "to learn?"
+	done
+	
+Text_DynamicpunchTutorTaught:
+	text "DYNAMICPUNCH is"
+	line "a FIGHTING attack"
+	cont "that can confuse"
+	cont "the foe!"
+	done
+	
+Text_DynamicpunchTutorRefused:
+	text "Very well!"
+	done
+	
+Text_DynamicpunchTutorClear:
+	text ""
+	done
 
 TrovitopolisDeptStore5F_MapEventHeader:
 
@@ -39,5 +255,9 @@ TrovitopolisDeptStore5F_MapEventHeader:
 	signpost 0, 14, SIGNPOST_READ, TrovitopolisDeptStore5FDirectory
 	signpost 0, 3, SIGNPOST_READ, TrovitopolisDeptStore5FElevatorButton
 
-.PersonEvents: db 1
-	person_event SPRITE_CLERK, 5, 8, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, PERSONTYPE_SCRIPT, 0, TrovitopolisTMMartScript, -1
+.PersonEvents: db 5
+	person_event SPRITE_CLERK, 5, 8, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_OW_RED, PERSONTYPE_SCRIPT, 0, TrovitopolisTMMartScript, -1
+	person_event SPRITE_BLACK_BELT,  5, 13, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, PAL_OW_BLUE, PERSONTYPE_SCRIPT, 0, Route52IcepunchTutor, -1
+	person_event SPRITE_BLACK_BELT,  2, 11, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1,  (1 << 3) | PAL_OW_RED, PERSONTYPE_SCRIPT, 0, Route52FirepunchTutor, -1
+	person_event SPRITE_BLACK_BELT,  2,  5, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, PAL_OW_PURPLE, PERSONTYPE_SCRIPT, 0, Route52CometpunchTutor, -1
+	person_event SPRITE_BLACK_BELT,  5,  3, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, PAL_OW_BROWN, PERSONTYPE_SCRIPT, 0, Route52DynamicPunchTutor, -1
